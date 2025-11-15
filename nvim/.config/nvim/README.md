71923c1bdb877b8a463ffbff0545d35aff7c4645
Michael <simmichaeld@gmail.com>
2025-11-14 21:34:46 -0500

    added nvim config

---
+++ nvim/.config/nvim/README.md
@@ -0,0 +1,5 @@
+# nvim
+
+my current nvim config.
+
+the base is a fork of [a modular version](https://github.com/dam9000/kickstart-modular.nvim) of [kickstart.nvim](https://github.com/nvim-lua/kickstart.nvim).
---
+++ nvim/.config/nvim/lua/lazy-plugins.lua
@@ -0,0 +1,28 @@
+-- [[ Configure and install plugins ]]
+require('lazy').setup({
+  --  NOTE: Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
+
+  { import = 'user.plugins' },
+}, {
+  ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
+
+-- vim: ts=2 sts=2 sw=2 et
