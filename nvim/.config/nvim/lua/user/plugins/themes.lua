5d1f4c2b3e86a5060be1f9b5b0ea88575617d634
Michael <simmichaeld@gmail.com>
2025-11-15 20:19:46 -0500

    set default theme to everforest

--- nvim/.config/nvim/lua/user/plugins/themes.lua
+++ nvim/.config/nvim/lua/user/plugins/themes.lua
@@ -1,21 +1,21 @@
 return {
-  -- {
-  --   'neanias/everforest-nvim',
-  --   version = false,
-  --   lazy = false,
-  --   priority = 1000, -- make sure to load this before all the other start plugins
-  --   -- Optional; default configuration will be used if setup isn't called.
-  --   config = function()
-  --     require('everforest').setup {
-  --       transparent_background_level = 2,
-  --     }
-  --     vim.cmd.colorscheme 'everforest'
-  --   end,
-  -- },
   {
-    'arcticicestudio/nord-vim',
+    'neanias/everforest-nvim',
+    version = false,
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- Optional; default configuration will be used if setup isn't called.
     config = function()
-      vim.cmd.colorscheme 'nord'
+      require('everforest').setup {
+        transparent_background_level = 2,
+      }
+      vim.cmd.colorscheme 'everforest'
     end,
   },
+  -- {
+  --   'arcticicestudio/nord-vim',
+  --   config = function()
+  --     vim.cmd.colorscheme 'nord'
+  --   end,
+  -- },
 }
